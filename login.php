3592c0d91efc17256bd07ab1caddff2f93e58a46
Vaidehi Karve <86217904+VaidehiKarve@users.noreply.github.com>
2021-12-17 11:10:06 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "127.0.0.1";
+    $user = "root";
+    $pass = "1234";
+    $db = "foo"; //name of db in workbench
+    $port = 3307;
+ ?>
--- page2.js
+++ page2.js
@@ -1,1 +1,19 @@
 object.onclick = function(){myScript};
+function mybrownieFunction(){
+    window.open("browniespage.html");
+}
+
+object.onclick = function(){myScript};
+function mycookieFunction(){
+    window.open("cookiespage.html");
+}
+
+object.onclick = function(){myScript};
+function mycakeFunction(){
+    window.open("cakepage.html");
+}
+
+object.onclick = function(){myScript};
+function myhomepageFunction(){
+    window.open("sem1finalwebsite.html");
+}
